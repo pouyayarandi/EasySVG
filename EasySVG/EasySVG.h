244f2898fa5366fa1fc6e51bc456234c399a8a82
pouya <p.yarandi@gmail.com>
2019-01-21 12:43:49 +0330

    Initial Commit

---
+++ EasySVG/EasySVG.h
@@ -0,0 +1,19 @@
+//
+//  EasySVG.h
+//  EasySVG
+//
+//  Created by Pouya on 1/21/19.
+//  Copyright © 2019 pouya yarandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasySVG.
+FOUNDATION_EXPORT double EasySVGVersionNumber;
+
+//! Project version string for EasySVG.
+FOUNDATION_EXPORT const unsigned char EasySVGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasySVG/PublicHeader.h>
+
+
